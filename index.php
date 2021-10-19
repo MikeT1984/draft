09356f9c5f45eed019e8d0afa5ad670fc584e907
Mike <wwwtitov@yandex.ru>
2021-10-19 18:40:20 +0300

    WarModel

--- index.php
+++ index.php
@@ -13,14 +13,30 @@ class Model
         $this->fuel -= $distance * 0.15;
         echo  "{$this->name} проехал {$distance} км топлива осталось {$this->fuel} литров <br>";
     }
-    public function info ()
+    public function info()
     {
-     echo " {$this->name} заправлен на {$this->fuel} литров горючего <br>";
+        echo " {$this->name} заправлен на {$this->fuel} литров горючего <br>";
     }
-    // public function attack(Tank $target){
-    // echo "{$this->name} выстрелил по {$target->$this->name} и нанес {$this->damage} урона <br> ."
-    // }
+}
 
+class WarModel extends Model
+{
+    public $armor;
+    public $damage;
+    public function __construct($name = null, $fuel = null, $armor = null, $damage = null)
+    {
+        parent::__construct($name, $fuel);
+        $this->damage = $damage;
+        $this->armor = $armor;
+    }
+    public function attack(Model $target)
+    {
+        $target->armor -= $this->damage;
+        echo "{$this->name} атаковал {$target->name} и нанес ему {$this->damage} урона. <br>";}
+    public function info(){
+        parent:: info();
+        echo "{$this->name} имеет броню в количестве {$this->armor} и силу атаки {$this->damage}. <br>";
+    }
 }
 $tractor = new Model("Трактор", 100);
 $tractor->info();
